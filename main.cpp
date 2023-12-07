06297da9ac696ccb906993dd2a375c78143d6350
Shya <keinelmora@gmail.com>
2023-12-07 18:41:00 -0400

    Small changes
    Se arregló el icono del ejecutable

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <QApplication>
 #include "mainwindow.h"
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QString dataPath = QCoreApplication::applicationDirPath() + QDir::separator() + "data";
+    QCoreApplication::addLibraryPath(dataPath);
+
     MainWindow mainWindow;
     mainWindow.setFixedSize(1000, 600);
     mainWindow.setWindowTitle("OneMath");
